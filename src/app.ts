f145f96185b23ec5c8761f13407017196243b59b
Imam-Hossain <zunayedemon6t9@gmail.com>
2023-05-23 19:59:16 +0600

    server ready

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, urlencoded } from 'express';
+import cors from 'cors';
+
+
+const app:Application = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({extended:true}));
+
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from './app';
+import mongoose from 'mongoose';
+const port:number = 5000;
+const run = async () => {
+    try{
+       await mongoose.connect('mongodb://127.0.0.1:27017/booksBD')
+       .then(()=> console.log("Db is successfully connected"))
+       .catch(err => console.log(err))
+
+    }
+    catch(err){
+        console.log(err)
+
+    }
+}
+run().catch(err => console.error(err))
+
+app.listen(port , ()=> {
+    console.log(`server is running on port ${port}`);
+})
